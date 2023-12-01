8e48cd7a78a36228ae6e8da8635904d90481472f
AlmantasK <almantusk@gmail.com>
2023-12-01 10:55:58 +0200

    typo

--- AdventOfCode/Day1/DigitsMatcher.cs
+++ AdventOfCode/Day1/DigitsMatcher.cs
@@ -32,53 +32,53 @@ namespace AdventOfCode.Day1
                 Word = number;
                 Matches = matches;
             }
-
-            public static string ReplaceTheFirstFoundDigitWordWithDigit(string line)
-            {
-                List<NumberLookup> numberTranslationLookups = LookupForNumbers(line);
-
-                var firstNumberFound = numberTranslationLookups
-                    .Where(l => l.Matched)
-                    .OrderBy(l => l.FirstMatchIndex)
-                    .FirstOrDefault();
-
-                if (firstNumberFound == null) return line;
-                return ReplaceLookupWordWithDigit(line, firstNumberFound.FirstMatchIndex, firstNumberFound.Digit, firstNumberFound.Word.Length);
-            }
-
-            public static string ReplaceTheLastFoundDigitWordWithDigit(string line)
-            {
-                List<NumberLookup> numberTranslationLookups = LookupForNumbers(line);
-
-                var lastNumberFound = numberTranslationLookups
-                    .Where(l => l.Matched)
-                    .OrderByDescending(l => l.LastMatchIndex)
-                    .FirstOrDefault();
-
-                if (lastNumberFound == null) return line;
-                return ReplaceLookupWordWithDigit(line, lastNumberFound.LastMatchIndex, lastNumberFound.Digit, lastNumberFound.Word.Length);
-            }
-
-            private static List<NumberLookup> LookupForNumbers(string line)
-            {
-                var numberTranslationLookups = new List<NumberLookup>();
-                foreach (var numberTranslation in NumberLookup.Numbers)
-                {
-                    var index = Regex.Matches(line, numberTranslation.Key);
-                    numberTranslationLookups.Add(new NumberLookup(numberTranslation.Key, index));
-                }
-
-                return numberTranslationLookups;
-            }
-
-            private static string ReplaceLookupWordWithDigit(string line, int index, string numberToBeReplaced, int lenghtOfNumber)
-            {
-                return
-                    line.Substring(0, index) +
-                    numberToBeReplaced +
-                    line.Substring(index + lenghtOfNumber);
-            }
-
         }
+
+        public static string ReplaceTheFirstFoundDigitWordWithDigit(string line)
+        {
+            List<NumberLookup> numberTranslationLookups = LookupForNumbers(line);
+
+            var firstNumberFound = numberTranslationLookups
+                .Where(l => l.Matched)
+                .OrderBy(l => l.FirstMatchIndex)
+                .FirstOrDefault();
+
+            if (firstNumberFound == null) return line;
+            return ReplaceLookupWordWithDigit(line, firstNumberFound.FirstMatchIndex, firstNumberFound.Digit, firstNumberFound.Word.Length);
+        }
+
+        public static string ReplaceTheLastFoundDigitWordWithDigit(string line)
+        {
+            List<NumberLookup> numberTranslationLookups = LookupForNumbers(line);
+
+            var lastNumberFound = numberTranslationLookups
+                .Where(l => l.Matched)
+                .OrderByDescending(l => l.LastMatchIndex)
+                .FirstOrDefault();
+
+            if (lastNumberFound == null) return line;
+            return ReplaceLookupWordWithDigit(line, lastNumberFound.LastMatchIndex, lastNumberFound.Digit, lastNumberFound.Word.Length);
+        }
+
+        private static List<NumberLookup> LookupForNumbers(string line)
+        {
+            var numberTranslationLookups = new List<NumberLookup>();
+            foreach (var numberTranslation in NumberLookup.Numbers)
+            {
+                var index = Regex.Matches(line, numberTranslation.Key);
+                numberTranslationLookups.Add(new NumberLookup(numberTranslation.Key, index));
+            }
+
+            return numberTranslationLookups;
+        }
+
+        private static string ReplaceLookupWordWithDigit(string line, int index, string numberToBeReplaced, int lenghtOfNumber)
+        {
+            return
+                line.Substring(0, index) +
+                numberToBeReplaced +
+                line.Substring(index + lenghtOfNumber);
+        }
+
     }
 }
